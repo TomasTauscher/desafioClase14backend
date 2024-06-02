dec754754a8676c4a24960463ae18649b8750109
TomasTauscher <tomastauscher@gmail.com>
2024-06-02 19:26:06 -0300

    firstCommit

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 import { Router } from "express";
 import productsRouters from "./productsRoutes.js"
 import cartsRouters from "./cartsRoutes.js"
+import sessionRouters from "./session.routes.js"
+import { isLogin } from "../middlewares/isLogin.middleware.js"
 
 const router = Router()
 
 
-router.use("/products", productsRouters)
+router.use("/products", isLogin, productsRouters)
 router.use("/carts", cartsRouters)
-
-
-
-
+router.use("/session", sessionRouters)
 
 
 
